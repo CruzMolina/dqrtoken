9b2673758c0f1701af87c1ef9925bed9266b14e3
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-03-04 12:54:25 -0800

    Refactor migrations scripts
    - Remove migrations script for Migrations.sol
    - Add separate migrations scripts for DQR10 & DQR30

---
+++ migrations/1_dqr_10_migration.js
@@ -0,0 +1,5 @@
+const DQR_10 = artifacts.require("DQR10");
+
+module.exports = deployer => {
+  deployer.deploy(DQR_10);
+};
---
+++ migrations/2_dqr_30_migration.js
@@ -0,0 +1,5 @@
+const DQR_30 = artifacts.require("DQR30");
+
+module.exports = deployer => {
+  deployer.deploy(DQR_30);
+};
