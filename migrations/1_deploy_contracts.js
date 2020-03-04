709fcde8b6a5d4fd3df873d46c09a2d4286175bb
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2020-03-04 13:04:02 -0800

    Refactor migrations scripts again
    - Streamline contract migrations into one migration script

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,5 +1,7 @@
 const DQR_10 = artifacts.require("DQR10");
+const DQR_30 = artifacts.require("DQR30");
 
 module.exports = deployer => {
   deployer.deploy(DQR_10);
+  deployer.deploy(DQR_30);
 };
--- migrations/2_dqr_30_migration.js
+++
@@ -1,5 +0,0 @@
-const DQR_30 = artifacts.require("DQR30");
-
-module.exports = deployer => {
-  deployer.deploy(DQR_30);
-};
